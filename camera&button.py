ec8066e525cc57b38943f34ed0d2b6ad8d607b17
Marianna <mariannaraven@gmail.com>
2018-11-27 13:11:22 +0300

    camera and button match updated

--- camera&button.py
+++ camera&button.py
@@ -11,21 +11,30 @@
 #       btn.get_state() - get state of button (0/False/GPIO.LOW or
 #                                               1/True/GPIO.HIGH)
 #       btn.clean() -   clean all GPIO pins
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 import cv2
 from flask import Flask, render_template, Response
+import requests
 
-from button_class import buttonRpi
+#from button_class import buttonRpi
 from camera_streaming import Camera
 
 
-btn = buttonRpi(37, new_callback_btn)
-event_from_button = btn.get_state()
+#btn = buttonRpi(37, new_callback_btn)
+#event_from_button = btn.get_state()
+server_adress = 'https://intelligent-nurse.azurewebsites.net/'
+test_adress = 'http://localhost:5000'
 
 # if event getted push frame date into server
+def server_rqs(server_adress):
+    r = requests.get(server_adress)
+    if (int(r.status_code) == 200):
+        return True
+    else:
+        return False
 
-def run_pushing(event_from_button):
+def run_pushing(event_from_button, server_adress):
     if event_from_button == True:
         app = Flask(__name__)
 
@@ -33,8 +42,9 @@ def run_pushing(event_from_button):
         def index(): return render_template('stream.html')
 
         def gen(camera):
-            while True:
+            while (server_rqs(server_adress)):
                 frame = camera.get_frame()
+                _, frame_encoded = cv2.imencode('.jpg', frame)
                 yeld(b'--frame\r\n'
                      b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
         @app.route('video_feed')
@@ -46,4 +56,4 @@ def run_pushing(event_from_button):
         # For rinning on Gosha's server inser host, port
         app.run(host='0.0.0.0', port=80, debug=True)
     else:
-        print('LOL!!! Press the button or die')
+        print('Press the button to start streaming')
